e84aa4bed852cc988211646cd8b0273bb0b7d41f
Juan Gabriel <tuxgabo@gmail.com>
2016-11-14 16:21:37 -0500

    some new tests

--- js/components/queryComponent.js
+++ js/components/queryComponent.js
@@ -1,8 +1,24 @@
 import React  from 'react'
-const queryComponent = ({result})=>{
+const QueryComponent = React.createClass({
+  getDefaultProps(){
+    return { query:{actualResult:5} }
+  },
+  render(){
+    const {result} = this.props
+    console.log("update not sure",result)
+    if(result == null){
+      return (<div>loading</div>)
+    }
+    else{
+      return (<div>this is the result
+                  {result.map((o)=>(<div>{o.result}</div>))}
+                  </div>)
+    }
+   
+  }
+})
+QueryComponent.propTypes = {
+  result :React.PropTypes.any
+}
 
-}
-queryComponent.defaultProps = {
-  query:{actualResult:5}
-}
-export default
+export default QueryComponent
