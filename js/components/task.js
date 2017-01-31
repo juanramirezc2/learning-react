0be75d8588fd682a7f47fb77361034ed8ef6bf75
Juan Gabriel <tuxgabo@gmail.com>
2017-01-31 13:17:29 -0500

    task style

--- js/components/task.js
+++ js/components/task.js
@@ -1,9 +1,10 @@
-import {PropTypes} from 'react'
-import React from 'react'
-import app from '../../css/app.css'
-const task = ({title,type})=><div className={app.app}>
-  {title}
-</div>
+import { PropTypes } from "react";
+import React from "react";
+import app from "../../css/app.css";
+const task = ({ title, type }) => (
+  <div className={app.task}>
+    {title}
+  </div>
+);
 
-
-export default task
+export default task;
