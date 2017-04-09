cd51d11374730086744b70f4bdc394cee94e23e2
Juan Gabriel <tuxgabo@gmail.com>
2017-04-09 10:57:43 -0500

    not sure what changes are these long time without coding

--- js/components/app.js
+++ js/components/app.js
@@ -4,6 +4,7 @@ import List from "./stateless/List";
 import ProvidedList from "./containers/taskProvider";
 import ProvidedHead from "./containers/addAndSelectTask";
 import { default as TouchBackend } from "react-dnd-touch-backend";
+import HTMLBackend from "react-dnd-html5-backend"
 import { DragDropContext } from "react-dnd";
 
 var App = () => (
@@ -12,5 +13,11 @@ var App = () => (
     <ProvidedList />
   </div>
 );
-
-export default DragDropContext(TouchBackend)(App);
+const appWrapped 
+if(window.innerWidth>768){
+  appWrapped = DragDropContext()
+}
+else {
+  appWrapped = DragDropContext(TouchBackend({ enableMouseEvents: true }))(App);
+}
+export default  appWrapped
