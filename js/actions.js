e90700a68d2994d05546988cd4baafda991e6ace
Juan Gabriel <tuxgabo@gmail.com>
2017-05-07 10:38:24 -0500

    notifications and vibrate test

--- js/actions.js
+++ js/actions.js
@@ -10,6 +10,51 @@ export const SETINITIAL = 'setInitial';
 export const DELETE = 'delete';
 export const REORDER = 'reorder';
 
+
+
+/*notifications creator ;) thinking towards push notifications*/
+  // function for creating the notification
+  function createNotification(title) {
+
+    // Let's check if the browser supports notifications
+    if (!"Notification" in window) {
+      console.log("This browser does not support notifications.");
+    }
+
+    // Let's check if the user is okay to get some notification
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      
+      var img = '/images/icons/icon-128x128.png';
+      var text = 'HEY! Your task "' + title + '" is now overdue.';
+      var notification = new Notification('To do list', { body: text, icon: img });
+      
+      window.navigator.vibrate(500);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    // Note, Chrome does not implement the permission static property
+    // So we have to check for NOT 'denied' instead of 'default'
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+
+        // Whatever the user answers, we make sure Chrome stores the information
+        if(!('permission' in Notification)) {
+          Notification.permission = permission;
+        }
+
+        // If the user is okay, let's create a notification
+        if (permission === "granted") {
+          var img = '/images/icons/icon-128x128.png';
+          var text = 'HEY! Your task "' + title + '" is now overdue.';
+          var notification = new Notification('To do list', { body: text, icon: img });
+          
+          window.navigator.vibrate(500);
+        }
+      });
+    }
+  }
+
 /*
 * action creators
 */
@@ -45,6 +90,7 @@ export function deleteTaskAction(taskId) {
   return dispatch => {
     deleteTask(taskId).onsuccess = e => {
        refreshRedux(dispatch)
+       createNotification("test Notifications")
     };
   };
 }
