b6cf6094dc3239c323baecfec008b279c97c2232
Juan Gabriel <tuxgabo@gmail.com>
2017-05-07 11:20:26 -0500

    v30

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-var cache_name = "my-site-cache-v29";
+var cache_name = "my-site-cache-v30";
 var urlsToCache = ["/", "/index.html", "/test.html","/bundle.js"];
 
 self.addEventListener("install", function(event) {
