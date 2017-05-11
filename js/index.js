dce978ef2e02940699e8a8f08384a98206e4df67
Juan Gabriel <tuxgabo@gmail.com>
2017-05-11 16:07:41 -0500

    service worker registration cleaned up

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-import "babel-polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import reducer from "./reducers";
-import { onerror, onsuccess, onupgradeneeded } from "./db";
-
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import { onerror, onsuccess, onupgradeneeded } from './db';
+import registerSw from './swRegistration';
+// register a sw
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', registerSw);
+}
 // Redux chrome extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // SERVICE WORKER NOT WEB WORKER
@@ -15,7 +19,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 //Indexed Db Stuff
-var request = window.indexedDB.open("reduxStore", 17);
+var request = window.indexedDB.open('reduxStore', 17);
 request.onerror = onerror;
 request.onsuccess = onsuccess(store);
 request.onupgradeneeded = onupgradeneeded;
@@ -23,5 +27,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("container")
+  document.getElementById('container')
 );
---
+++ js/swRegistration.js
@@ -0,0 +1,44 @@
+/* TODO: clean up this */
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+function RegisterSw() {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function(ServiceWorkerRegistration) {
+      // Registration was successful
+      console.log(
+        'ServiceWorker registration successful with scope: ',
+        ServiceWorkerRegistration.scope
+      );
+      const subscribeOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          'BNVFaEer_S5bnKoFlwJjf5wzW7EUCIWzYtu6wVw_mnwWVCukMNdwyC1grHaQSfVEeDIFohbgw47tncBHaNa9AZE'
+        )
+      };
+      return ServiceWorkerRegistration.pushManager.subscribe(subscribeOptions);
+    })
+    .then(function(pushSubscription) {
+      return fetch(`/api/save-subscription`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(pushSubscription)
+      });
+    })
+    .catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
+export default RegisterSw;
