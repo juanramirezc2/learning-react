3b559696cd0571da2de7b70d7c7364cbb5a62c83
Juan Gabriel <tuxgabo@gmail.com>
2017-05-12 17:26:02 -0500

    compose redux

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
