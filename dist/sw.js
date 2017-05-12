f98861f50c0572212b9dae027db8b31761b47d85
gabo <tuxgabo@gmail.com>
2017-05-12 22:27:53 +0000

    cache flushed

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-var cache_name = 'my-site-cache-v38';
+var cache_name = 'my-site-cache-v39';
 var urlsToCache = ['/', '/index.html', '/test.html', '/bundle.js'];
 
 self.addEventListener('install', function(event) {
